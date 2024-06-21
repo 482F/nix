30897199e3dd21201c44b7aa8f315c6569d04655
482F <482F.git.mail@482f.net>
2024-06-22 00:00:00 +0900

    acd の設定をファイルに切り出し

--- home/misc.nix
+++ home/misc.nix
@@ -87,25 +87,6 @@
       bind '"\C-jd":"pushd +1 > /dev/null 2>&1 && popd -0 -n > /dev/null 2>&1 && pwd"'
       bind '"\C-jl":"dirs -p -v"'
       bind -x '"\C-x\C-f":"fg"'
-
-      export ACD_PORT=55812
-
-      PATH_TO_ACD="/home/${user}/git/misc/acd.ts"
-      alias acdts="''${PATH_TO_ACD}"
-      function acd() {
-        if [[ "''${1:-}" == "completions" ]]; then
-          acdts "''${@:1}"
-          return
-        fi
-        local result=$(acdts "''${@}")
-        if [[ "''${result}" == "cd"* ]]; then
-          source <(echo "''${result}")
-        elif [[ "''${result}" != "" ]]; then
-          echo "''${result}"
-        fi
-      }
-      (nohup "''${PATH_TO_ACD}" --listen >/dev/null 2>&1 &)
-      source <(acdts completions bash | perl -pe 's/_acd_complete alias/COMP_WORDS=\"\''${COMP_WORDS[@]}\" COMP_CWORD=\"\''${COMP_CWORD}\" _acd_complete alias/')
     '';
   };
 
