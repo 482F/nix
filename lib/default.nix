9849dfcd66e87872e7b6534779adda48c8c7fa4d
482F <482F.git.mail@482f.net>
2024-06-22 00:00:00 +0900

    nix 適用時に git リポジトリをクローンするモジュールを作成

--- lib/default.nix
+++ lib/default.nix
@@ -21,4 +21,29 @@
   in
     pkgs.writeScriptBin binName ''nix run nixpkgs#${pkgName} -- "''${@}"'';
   importAll = with builtins; path: map (pathStr: import (path + ("/" + pathStr))) (attrNames (readDir path));
+
+  # `imports = [(myLib.gitClone { ... })]`
+  gitClone = {
+    homeManagerLib,
+    cloneRemote,
+    finalRemote ? cloneRemote,
+    dist,
+  }: {
+    home.activation = builtins.listToAttrs [
+      {
+        name = "git-clone-" + dist;
+        value = homeManagerLib.dag.entryAfter ["writeBoundary"] ''
+          function _() {
+            if [[ -d '${dist}' ]]; then
+              return 0
+            fi
+            run nix shell nixpkgs#git nixpkgs#openssh --command git clone '${cloneRemote}' '${dist}'
+            cd '${dist}'
+            run nix shell nixpkgs#git --command git remote set-url origin '${finalRemote}'
+          }
+          _; unset -f _
+        '';
+      }
+    ];
+  };
 }
