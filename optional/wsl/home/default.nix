85e06682652a5ed807393ff98e91050c67a25009
482F <482F.git.mail@482f.net>
2024-06-22 00:00:00 +0900

    psh と wsl を packages 化

--- optional/wsl/home/default.nix
+++ optional/wsl/home/default.nix
@@ -6,17 +6,20 @@
   user,
   ...
 }: {
+  home.packages = with builtins;
+    attrValues (mapAttrs myLib.writeScriptBinWithArgs {
+      # TODO: $WINDIR を動的に取得したい
+      psh = "/mnt/c/windows/System32/WindowsPowerShell/v1.0/powershell.exe";
+      wsl = "/mnt/c/windows/system32/wsl.exe";
+    });
+
   programs.bash = {
     enable = true;
-    shellAliases = {
-      psh = "powershell.exe";
-    };
     initExtra = ''
       if [[ $PATH != *:/bin:* ]]; then
         export PATH="$PATH:/bin"
         function winpath() {
-          local psh="/mnt/c/windows/System32/WindowsPowerShell/v1.0/powershell.exe"
-          local winpath="$("$psh" echo '$env:PATH')"
+          local winpath="$(psh echo '$env:PATH')"
           local winpath_w="$(echo "''${winpath//\\/\/}" | grep -Po "[^;\r\n]+" | xargs -I {} wslpath -u {})"
 
           local path="$PATH"
