458bd2752048ce872e02d52b9686f768ccc930ec
482F <482F.git.mail@482f.net>
2024-07-05 00:00:00 +0900

    ssh 秘密鍵が 0 個のときにエラーになっていたのを修正

--- home/ssh.nix
+++ home/ssh.nix
@@ -13,24 +13,27 @@
     compression = true;
   };
 
-  programs.bash = {
-    enable = true;
-    initExtra = ''
-      function ssh-add-if-necessary() {
-        while read -u 10 _keyfile; do
-          local keyfile="''${_keyfile// /}"
-          local pub="$(ssh-keygen -l -f "$keyfile" | grep -Po "^\S+\s\S+")"
-          # skip if already exists
-          if (ssh-add -l 2>/dev/null || true) | grep -q "$pub"; then
-            continue
-          fi
-          ssh-add "$keyfile"
-        done 10<< "  END"
-          ${builtins.concatStringsSep "\n" env.ssh.secretKeys.${user}}
-        END
-      }
-      ssh-add-if-necessary
-      unset -f ssh-add-if-necessary
-    '';
-  };
+  programs.bash =
+    if (env.ssh.secretKeys.${user} or null) == null
+    then {}
+    else {
+      enable = true;
+      initExtra = ''
+        function ssh-add-if-necessary() {
+          while read -u 10 _keyfile; do
+            local keyfile="''${_keyfile// /}"
+            local pub="$(ssh-keygen -l -f "$keyfile" | grep -Po "^\S+\s\S+")"
+            # skip if already exists
+            if (ssh-add -l 2>/dev/null || true) | grep -q "$pub"; then
+              continue
+            fi
+            ssh-add "$keyfile"
+          done 10<< "  END"
+            ${builtins.concatStringsSep "\n" (env.ssh.secretKeys.${user} or [])}
+          END
+        }
+        ssh-add-if-necessary
+        unset -f ssh-add-if-necessary
+      '';
+    };
 }
