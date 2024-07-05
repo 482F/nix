fb95b871c0d6dc251c9c6c4d52d2f11fda4fdbef
482F <482F.git.mail@482f.net>
2024-07-06 00:00:00 +0900

    bash-history-commit で my.systemd を使用するように

--- home/bash-history.nix
+++ home/bash-history.nix
@@ -6,55 +6,36 @@
   user,
   ...
 }: {
-  systemd.user.timers.bash-history-commit = {
-    Install = {
-      WantedBy = ["timers.target"];
-    };
-    Timer = {
-      OnCalendar = "*-*-* 15:00:00";
-    };
-  };
-  systemd.user.services.bash-history-commit = {
-    Unit = {
-      Description = "git commit bash_history";
-    };
-    Service = {
-      Type = "oneshot";
-      ExecStart =
-        (
-          pkgs.writeScriptBin "bash-history-commit" ''
-            #!${pkgs.bash.outPath}/bin/bash --login
+  my.systemd.timer.bash-history-commit = {
+    description = "git commit bash_history";
+    onCalendar = ["*-*-* 15:00:00"];
+    script = ''
+      set -ue -o pipefail
 
-            set -ue -o pipefail
+      if [[ ! -f ~/.bash_history ]]; then
+        exit 0
+      fi
 
-            if [[ ! -f ~/.bash_history ]]; then
-              exit 0
-            fi
+      bash_hist_dir=~/git/bash_history
+      if [[ ! -d "$bash_hist_dir" ]]; then
+        mkdir -p "$bash_hist_dir"
+      fi
 
-            bash_hist_dir=~/git/bash_history
-            if [[ ! -d "$bash_hist_dir" ]]; then
-              mkdir -p "$bash_hist_dir"
-            fi
+      cd "$bash_hist_dir"
 
-            cd "$bash_hist_dir"
+      git="${pkgs.git.outPath}/bin/git"
+      if [[ ! -d "$bash_hist_dir/.git" ]]; then
+        "$git" init
+      fi
 
-            git="${pkgs.git.outPath}/bin/git"
-            if [[ ! -d "$bash_hist_dir/.git" ]]; then
-              "$git" init
-            fi
+      if [[ ! -f "$bash_hist_dir/.bash_history" ]]; then
+        mv ~/.bash_history ./
+        ln -s "$(pwd)/.bash_history" ~/.bash_history
+      fi
 
-            if [[ ! -f "$bash_hist_dir/.bash_history" ]]; then
-              mv ~/.bash_history ./
-              ln -s "$(pwd)/.bash_history" ~/.bash_history
-            fi
-
-            name=nix
-            email=nix@example.com
-            git add .; GIT_AUTHOR_NAME=$name GIT_AUTHOR_EMAIL=$email GIT_COMMITTER_NAME=$name GIT_COMMITTER_EMAIL=$email git commit -m "commit .bash_history"
-          ''
-        )
-        .outPath
-        + "/bin/bash-history-commit";
-    };
+      name=nix
+      email=nix@example.com
+      git add .; GIT_AUTHOR_NAME=$name GIT_AUTHOR_EMAIL=$email GIT_COMMITTER_NAME=$name GIT_COMMITTER_EMAIL=$email git commit -m "commit .bash_history"
+    '';
   };
 }
