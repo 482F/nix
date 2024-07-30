193131692c576936db1e51616299bf4a5479420d
482F <482F.git.mail@482f.net>
2024-07-31 00:00:00 +0900

    nix-direnv を追加

---
+++ home/direnv.nix
@@ -0,0 +1,14 @@
+{
+  config,
+  pkgs,
+  env,
+  ...
+}: {
+  programs = {
+    direnv = {
+      enable = true;
+      enableBashIntegration = true;
+      nix-direnv.enable = true;
+    };
+  };
+}
