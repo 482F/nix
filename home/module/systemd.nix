845a2f47809ea5410727840f4da9da5d0e96389c
482F <482F.git.mail@482f.net>
2024-08-04 00:00:00 +0900

    my.systemd.timer に enable オプションを追加

--- home/module/systemd.nix
+++ home/module/systemd.nix
@@ -3,15 +3,20 @@
   lib,
   pkgs,
   user,
+  myLib,
   ...
 }: let
-  inherit (lib) mkOption types;
+  inherit (lib) mkOption mkEnableOption types;
 in {
   options = {
     my.systemd.timer = mkOption {
       default = {};
       type = types.attrsOf (types.submodule {
         options = {
+          enable = mkOption {
+            type = types.bool;
+            default = true;
+          };
           description = mkOption {type = types.str;};
           onCalendar = mkOption {type = types.listOf types.str;};
           script = mkOption {type = types.str;};
@@ -21,25 +26,29 @@ in {
   };
 
   config.systemd.user.timers =
-    builtins.mapAttrs (name: value: {
-      Install = {WantedBy = ["timers.target"];};
-      Timer = {OnCalendar = value.onCalendar;};
-    })
-    config.my.systemd.timer;
+    builtins.mapAttrs (
+      name: value: {
+        Install = {WantedBy = ["timers.target"];};
+        Timer = {OnCalendar = value.onCalendar;};
+      }
+    )
+    (myLib.filterAttrs (name: value: value.enable) config.my.systemd.timer);
   config.systemd.user.services =
-    builtins.mapAttrs (name: value: {
-      Unit = {Description = value.description;};
-      Service = {
-        Type = "oneshot";
-        ExecStart =
-          (pkgs.writeScriptBin name (''
-              #!${pkgs.bash}/bin/bash --login
-            ''
-            + value.script))
-          .outPath
-          + "/bin/"
-          + name;
-      };
-    })
-    config.my.systemd.timer;
+    builtins.mapAttrs (
+      name: value: {
+        Unit = {Description = value.description;};
+        Service = {
+          Type = "oneshot";
+          ExecStart =
+            (pkgs.writeScriptBin name (''
+                #!${pkgs.bash}/bin/bash --login
+              ''
+              + value.script))
+            .outPath
+            + "/bin/"
+            + name;
+        };
+      }
+    )
+    (myLib.filterAttrs (name: value: value.enable) config.my.systemd.timer);
 }
