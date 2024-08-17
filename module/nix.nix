0824358752d2b0b26bf9a1a83e6009c77f272951
482F <482F.git.mail@482f.net>
2024-08-18 00:00:00 +0900

    nix subcommand で bash 補完が正常に動くように

--- module/nix.nix
+++ module/nix.nix
@@ -11,18 +11,47 @@
         targetDerivation = myLib.writeScriptBin "nix" ''
           subcommand="''${1:-}"
           bin="_nix-$subcommand"
-          if [[ -n "$subcommand" ]] && which "$bin" > /dev/null 2>&1; then
+
+          ngc="''${NIX_GET_COMPLETIONS:-}"
+          if [[ "$ngc" != "1" ]] && [[ -n "$subcommand" ]] && which "$bin" > /dev/null 2>&1; then
             shift 1
           else
             bin="${pkgs.nix.outPath}/bin/nix"
           fi
           "$bin" "$@"
-          exit $?
+          code=$?
+
+          if [[ "$ngc" == "1" ]]; then
+            compgen -A command _nix-$subcommand | sed 's/^_nix-//g'
+          fi
+
+          exit $code
         '';
         binName = "nix";
-        runtimeDepDerivations = [
-          (myLib.writeScriptBinWithArgs "_nix-ch" ''sudo NIXPKGS_ALLOW_UNFREE=1 NIXPKGS_ALLOW_INSECURE=1 nixos-rebuild switch --flake "$(readlink ~/nix)#my-nixos" --impure'')
-        ];
+        runtimeDepDerivations = with builtins;
+          attrValues (mapAttrs (subcommand: {
+              script,
+              completion ? ":",
+            }:
+              myLib.writeScriptBin "_nix-${subcommand}" ''
+                function comp() {
+                  ${completion}
+                }
+                function main() {
+                  ${script}
+                }
+                if [[ "''${NIX_GET_COMPLETIONS:-}" != "" ]]; then
+                  comp "$@"
+                  exit 0
+                fi
+
+                main "$@"
+              '')
+            {
+              ch = {
+                script = ''sudo NIXPKGS_ALLOW_UNFREE=1 NIXPKGS_ALLOW_INSECURE=1 nixos-rebuild switch --flake "$(readlink ~/nix)#my-nixos" --impure'';
+              };
+            });
       })
     ];
     my.gc.nix.script = ''
