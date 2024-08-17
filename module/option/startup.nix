6d57a2864a3e22f0fbbc9b1f1b05fe3c88dad0ec
482F <482F.git.mail@482f.net>
2024-08-18 00:00:00 +0900

    my.startup.script が存在しないとエラーになっていたので修正

--- module/option/startup.nix
+++ module/option/startup.nix
@@ -17,54 +17,63 @@
               type = types.bool;
               default = true;
             };
-            script = mkOption {type = types.lines;};
+            script = mkOption {
+              type = types.lines;
+              default = "";
+            };
           };
         };
       };
     };
 
-    config.home.file."startup.sh" =
-      if config.my.startup.enable && (config.my.startup.script or null != null)
-      then {
-        executable = true;
-        text = config.my.startup.script;
+    imports = let
+      enable = config.my.startup.enable && (config.my.startup.script != "");
+    in [
+      {
+        config.home.file."startup.sh" =
+          if enable
+          then {
+            executable = true;
+            text = config.my.startup.script;
+          }
+          else {};
+
+        config.systemd.user.services =
+          if enable
+          then {
+            execute-startup = {
+              Unit = {Description = "execute startup script";};
+              Install = {WantedBy = ["default.target"];};
+              Service = {
+                Type = "oneshot";
+                ExecStart =
+                  (
+                    pkgs.writeScriptBin "startup" ''
+                      #!${pkgs.bash}/bin/bash
+
+                      startup="$HOME/startup.sh"
+                      if [[ ! -f "$startup" ]]; then
+                        exit 0
+                      fi
+
+                      suppress="/tmp/suppress-startup"
+                      if [[ -f "$suppress" ]]; then
+                        ${pkgs.coreutils}/bin/rm "$suppress"
+                        exit 0
+                      fi
+
+                      ${pkgs.bash.outPath}/bin/bash --login "$startup"
+
+                      while true; do ${pkgs.coreutils}/bin/sleep infinity; done
+                    ''
+                  )
+                  .outPath
+                  + "/bin/startup";
+              };
+            };
+          }
+          else {};
       }
-      else {};
-
-    config.systemd.user.services =
-      if config.my.startup.enable && (config.my.startup.script or null != null)
-      then {
-        execute-startup = {
-          Unit = {Description = "execute startup script";};
-          Install = {WantedBy = ["default.target"];};
-          Service = {
-            Type = "oneshot";
-            ExecStart =
-              (
-                pkgs.writeScriptBin "startup" ''
-                  #!${pkgs.bash}/bin/bash
-
-                  startup="$HOME/startup.sh"
-                  if [[ ! -f "$startup" ]]; then
-                    exit 0
-                  fi
-
-                  suppress="/tmp/suppress-startup"
-                  if [[ -f "$suppress" ]]; then
-                    ${pkgs.coreutils}/bin/rm "$suppress"
-                    exit 0
-                  fi
-
-                  ${pkgs.bash.outPath}/bin/bash --login "$startup"
-
-                  while true; do ${pkgs.coreutils}/bin/sleep infinity; done
-                ''
-              )
-              .outPath
-              + "/bin/startup";
-          };
-        };
-      }
-      else {};
+    ];
   };
 }
