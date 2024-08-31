1c4d1aac648a371e98c6643e94a90fedd2594c95
482F <482F.git.mail@482f.net>
2024-09-01 00:00:00 +0900

    yarn16 を除去

--- optional/nodejs/default.nix
+++ optional/nodejs/default.nix
@@ -7,32 +7,7 @@
     lib,
     ...
   }: {
-    home.packages =
-      map (
-        {
-          suffix,
-          nodeVersion,
-          nodeSha256,
-        }: let
-          buildNodejs = pkgs.callPackage <nixpkgs/pkgs/development/web/nodejs/nodejs.nix> {python = pkgs.python310;};
-          nodejs = buildNodejs {
-            version = nodeVersion;
-            sha256 = nodeSha256;
-          };
-          yarn = pkgs.yarn.override {
-            inherit nodejs;
-          };
-        in
-          myLib.writeScriptBinWithArgs "yarn${suffix}" "${yarn}/bin/yarn"
-      ) [
-        {
-          suffix = "16";
-          nodeVersion = "16.20.2";
-          nodeSha256 = "sha256-V28aA8RV5JGo0TK1h+trO4RlH8iXS7NjhDPdRNIsj0k=";
-        }
-      ]
-      ++ [pkgs.yarn];
-
+    home.packages = [pkgs.yarn];
     my.gc.yarn.script = ''rm -rf ~/.cache/yarn/*'';
   };
 }
